deb35ee4e802b71b634744feb57139587e5f815e
Leo Puvilland <leo@craftcat.dev>
2023-01-30 10:59:52 -0800

    add engine

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
+from entity import Entity
 from input_handlers import EventHandler
 
 
@@ -10,8 +11,7 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    
     
 
     tileset = tcod.tileset.load_tilesheet(
@@ -19,6 +19,13 @@ def main() -> None:
     )
 
     event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+
+    entities = {npc, player}
+
+    engine = Engine(entities=entities, event_hander=event_handler, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
@@ -29,25 +36,13 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
+            engine.render(console=root_console, context=context)
 
-            context.present(root_console)
+            events = tcod.event.wait()
 
-            root_console.clear()
+            engine.handle_events(events)
 
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
 
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
-    
 
 if __name__ == "__main__":
     main()
