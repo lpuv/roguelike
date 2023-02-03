37c7f75e964f402337ee2361944542456cf78f80
Leo Puvilland <leo@craftcat.dev>
2023-02-03 09:42:38 -0800

    add procedurally generated map and FOV

--- engine.py
+++ engine.py
@@ -2,34 +2,51 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
-from actions import EscapeAction, MovementAction
 from entity import Entity
+from game_map import GameMap
 from input_handlers import EventHandler
 
-class Engine:
-    def __init__(self, entities: Set[Entity], event_hander: EventHandler, player: Entity):
-        self.entities = entities
-        self.event_hander = event_hander
-        self.player = player
 
-    def handle_events(self, events: Iterable[Any]):
+class Engine:
+    def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
+        self.entities = entities
+        self.event_handler = event_handler
+        self.game_map = game_map
+        self.player = player
+        self.update_fov()
+
+    def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
-            action = self.event_hander.dispatch(event)
+            action = self.event_handler.dispatch(event)
 
             if action is None:
                 continue
 
-            if isinstance(action, MovementAction):
-                self.player.move(dx=action.dx, dy=action.dy)
+            action.perform(self, self.player)
+            
+            self.update_fov() # Update the FOV before the player's next action.
 
-            elif isinstance(action, EscapeAction):
-                raise SystemExit()
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the player's point of view"""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is "visible" it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
 
     def render(self, console: Console, context: Context) -> None:
+        self.game_map.render(console)
+
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            # Only print entities that are in FOV
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
         console.clear()
+
--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import tcod
 
 from engine import Engine
 from entity import Entity
 from input_handlers import EventHandler
-
+from procgen import generate_dungeon
 
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    
-    
+    map_width = 80
+    map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -22,16 +26,24 @@ def main() -> None:
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
-
     entities = {npc, player}
 
-    engine = Engine(entities=entities, event_hander=event_handler, player=player)
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
+
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Leo's roguelike",
+        title="Leo's Roguelike",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
@@ -43,8 +55,6 @@ def main() -> None:
             engine.handle_events(events)
 
 
-
 if __name__ == "__main__":
     main()
-
 
